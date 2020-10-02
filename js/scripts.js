127d5063a53a8347785afaab48762b38e76577cd
EveNyarango <evenyarango@gmail.com>
2020-10-02 18:44:14 +0300

    edit the form and js style

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
 var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var dayOfTheWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(dayOfTheWeek)
 
 function myFunction(){
   var date = document.getElementById("date").value;
-  var month = document.getElementById("month").value;
-  // var invalidMonth = ( month<1 || month>12 || ((document.getElementById("month").value)=="") );  
+  var month = document.getElementById("month").value; 
   var year = document.getElementById("year").value;
-  var male = document.getElementById("male").value;
-  var female = document.getElementById("female").value;
+  var gender = document.getElementById("gender").value;
+  
+
 
 var CC = parseInt(year.substr(0,2));
 var YY = parseInt(year.substr(2,3));
 var MM = month;
 var DD = date;
 var d = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
-alert(" Please,fill in all the requirements to proceed on.");
-return false;
-if (gender === "male"){
-  alert("For a male born on" + dayOfTheWeek[date.getDay()] + "," + "your Akan name is" + maleNames[date.getDay()] +","
-  );
-}if else (gender==="female"){
-  alert("For a male born on" + dayOfTheWeek[date.getDay()] + "," + "your Akan name is" + maleNames[date.getDay()] +","
-  );
 
+if (gender === "Male"){
+  alert("For a male born on  " + dayOfTheWeek[d] + ", " + "your Akan name is " + maleNames[d] +","
+  );
+  
+}else if(gender==="Female"){
+  alert("For a female born on " + dayOfTheWeek[d] + ", " + "your Akan name is " + femaleNames[d] +","
+  );
+  
+}else{
+alert("For the one born on " + "undefined date " + "your Akan name is" + "," + "undefined") ;
 }
 
 
